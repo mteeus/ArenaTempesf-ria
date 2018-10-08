a20678191e723c9600af0bd2ebfbcf482b39e3cb
guilherme294 <43962938+guilherme294@users.noreply.github.com>
2018-10-08 16:48:33 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -2,10 +2,10 @@ public class Teste{
     public static void main (String[] args) {
         System.out.printLn("Matheus Lima Santos ");
         System.out.printLn(" RA: 31725192");
-		System.out.printLn("Jean Carlo Medeiros Soares");
+	System.out.printLn("Jean Carlo Medeiros Soares");
         System.out.printLn(" RA: 318114534");
-	    stem.out.printLn("Marcela Alessandra Rodrigues");
-        System.out.printLn(" RA: 31722084 ");
+	System.out.printLn("Guilherme Eloizio Gomes Costa");
+        System.out.printLn(" RA: 31728587");
     }
 }
 ..
